179d9528751b50f3c3f9ddc16b4df7ff5d6b8fd2
Jeyi Adole <erthnice@gmail.com>
2022-04-02 10:08:37 +0100

    Edited ampersand

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <header>
-        <h2>Cards &#38; Currencies</h2>
+        <h2>{'Cards & Currencies'}</h2>
       </header>
       <div className=' container'>
         <div className='cards-list'>
